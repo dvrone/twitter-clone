56de84457cb9b73fa3fba1aab49af487b68bf239
dvrone <dvrk.devx@gmail.com>
2026-07-09 13:31:39 +0500

    Auto-create instance folder on startup; fix deployment on fresh environments

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -10,6 +11,8 @@ login_manager = LoginManager()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    os.makedirs(app.instance_path, exist_ok=True)
 
     db.init_app(app)
     login_manager.init_app(app)
